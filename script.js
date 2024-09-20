e2adbffe435bcbe199fa774198a7073d3d2c7fca
Arif Hussain <arifh9821@gmail.com>
2024-09-20 22:26:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function insert_Row() {
     //Write your code here
+  const table = document.getElementById('sampleTable');
   
+  const newRow =table.insertRow(0);
+  const Cell1 =newRow.insertCell(0);
+  const Cell2 =newRow.insertCell(1);
   
+  Cell1.innerHTML = "New Cell1";
+  Cell2.innerHTML = "New Cell2";
 }
